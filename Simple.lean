12ba3f3ec854036793a91c56e88cab01839fcce6
anirudhsk <sk.anirudh@gmail.com>
2025-11-20 10:45:05 -0500

    Commit some WIP.

--- Simple.lean
+++ Simple.lean
@@ -3,6 +3,8 @@ import Mathlib.Analysis.SpecialFunctions.Exp
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.FundThmCalculus
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.IntegrationByParts
+import Mathlib.Topology.Basic
+import Mathlib.Order.Filter
 
 open scoped Real Topology Interval
 open MeasureTheory
@@ -16,5 +18,6 @@ theorem deriv_Phi (x : ℝ) : deriv Phi x = gaussian x := by
   have h_meas : Measurable gaussian := by sorry
   have h_int_univ : Integrable gaussian := by sorry
   have h_finite_deriv : ∀ a, HasDerivAt (fun y => ∫ t in a..y, gaussian t) (gaussian x) x := by sorry
-  have h_deriv : HasDerivAt Phi (gaussian x) x := by sorry
+  have h_deriv : HasDerivAt Phi (gaussian x) x := by
+    have h_lim : Tendsto (fun a => ∫ t in a..x, gaussian t) atBot (𝓝 (∫ t in (−∞ : ℝ)..x, gaussian t)) :=
   simpa [Phi] using h_deriv.deriv
