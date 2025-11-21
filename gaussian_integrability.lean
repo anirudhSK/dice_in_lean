61a234554cb03e4730854bd0caf7fc843c7c11e8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2025-11-21 18:07:47 -0500

    Clean up proof a bit.

--- gaussian_integrability.lean
+++ gaussian_integrability.lean
@@ -8,9 +8,9 @@ noncomputable def gaussian (x : ℝ) : ℝ := Real.exp (- x^2 / 2)
 open MeasureTheory
 theorem gaussian_integrableOn : IntegrableOn gaussian (Set.univ : Set ℝ) volume := by
   have hb : 0 < (1 / 2 : ℝ) := by norm_num
-  have h0 : -1 < (0 : ℝ) := by norm_num
-   -- integrable on (0, ∞)
-  have h_int := integrableOn_rpow_mul_exp_neg_mul_sq hb h0
+  -- integrable on (0, ∞)
+  have h_int : IntegrableOn (fun x => Real.exp (-(1 / 2) * x ^ 2)) (Set.Ioi 0) volume:= by
+     exact (integrableOn_Ioi_exp_neg_mul_sq_iff).mpr hb
   -- this gives integrability on the set Ioi 0; but our function is even, so we can extend to all ℝ
   -- integrable on (-∞, 0) by change of variable x ↦ -x
   have h_neg : IntegrableOn (fun x => Real.exp (-(1 / 2) * x ^ 2)) (Set.Iio 0) := by sorry
