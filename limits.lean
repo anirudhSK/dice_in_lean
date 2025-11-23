f4971dfad0a8763151f7722635d20254baf84cc6
anirudhsk <sk.anirudh@gmail.com>
2025-11-22 21:44:56 -0500

    Remove old, but instructive limits.lean example.

--- limits.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib.Order.Filter.Basic
-import Mathlib.Topology.Basic
-import Mathlib.Topology.UniformSpace.Real
-
-example : Filter.Tendsto (fun x : ℝ => x) (nhds 0) (nhds 0) :=
-  Filter.tendsto_id
